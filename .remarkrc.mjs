08c357d47d45304528cdbffd29034d5c9c2ea59b
Daniel Mundra <daniel.mundra@civicactions.com>
2022-10-28 06:51:31 -0700

    Added remark-gfm to resolve sentence spacing in table warning.

--- .remarkrc.mjs
+++ .remarkrc.mjs
@@ -2,12 +2,14 @@ import remarkRetext from "remark-retext";
 import retextSentenceSpacing from "retext-sentence-spacing";
 import retextEnglish from "retext-english";
 import unified from "unified";
+import remarkGfm from "remark-gfm";
 
 const config = {
   plugins: [
     // Load retext linters to lint English.
     [
       // Parse the Markdown as English using retext.
+      remarkGfm,
       remarkRetext,
       unified().use(retextEnglish).use(retextSentenceSpacing),
     ],
